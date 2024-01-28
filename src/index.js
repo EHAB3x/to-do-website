774c8ebd4cff1f6791e882a74be788520ca12ec0
EHAB3x <ehabmah6969@gmail.com>
2024-01-28 14:26:47 +0200

    convert to hashRouter and change small screen font

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import Store from './state/Store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={Store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
